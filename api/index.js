7ceae689645a6bbca69ec9854e73a5d8bb055510
Joy Biswas <contact.joybiswas@gmail.com>
2023-03-19 20:46:45 +0600

    Basic Set up done

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const { Mongoose, default: mongoose } = require('mongoose');
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth')
+
+// use packages 
+dotenv.config();
+app.use(express.json());
+// ports 
+const port = process.env.PORT||3000;
+
+// Connect MongoDb 
+async function main() {
+    await mongoose.connect(process.env.MONGO_STRING);
+    console.log('Connected To DB');
+  }
+  main().catch(err => console.log(err));
+
+//   Use Routes 
+  app.use('/api/users',userRoute);
+  app.use('/api/auth',authRoute)
+
+
+
+
+
+
+
+// API Ends 
+
+app.listen(port, () => {
+  console.log(`The server lisining to port: ${port}`)
+})
